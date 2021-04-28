851119f2b13448525aac5b8147bc8f41bff385e4
azeemahmedkhan <k180194@nu.edu.pk>
2021-04-28 09:44:29 +0500

    updated database attributes

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cnproject', 
         'USER': 'postgres', 
-        'PASSWORD': 'wssoj3Fa',
+        'PASSWORD': os.environ.get('db_password'),
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
